86f4e62ca8212467ec35a223c8dc6928afdf1675
Clair <clair_griffiths@hotmail.com>
2016-11-01 19:10:52 +0000

    saving before nitrous goes down

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,11 +1,14 @@
 require 'flickr'
 
 class PhotosController < ApplicationController
-  
+
   def index
    Flickr.login_to_flickr
-   @info = flickr.photos.getInfo(:photo_id => "26297297126")
-   @url = "http://farm#{@info["farm"]}.staticflickr.com/#{@info["server"]}/#{@info["id"]}_#{@info["secret"]}.jpg"
+   #@info = flickr.people.getPhotos(:user_id => "125584460@N03", :per_page => 10)
+   # @tag = "Christmas"
+  # @info = flickr.photos.search(:user_id => "125584460@N03", :privacy_filter => 5, :per_page => 10, :tags => "Christmas")
+    @exif = flickr.photos.getInfo(:photo_id => 25564773643)
+    @photos = flickr.people.getInfo(:user_id => "125584460@N03")
   end
-  
+
 end
--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -1,2 +1,11 @@
 module PhotosHelper
+  
+  def get_photos(tag)
+    flickr.photos.search(:user_id => "125584460@N03", :privacy_filter => 5, :per_page => 10, :tags => tag)
+  end
+  
+  def get_info(id)
+    flickr.photos.getInfo(:photo_id => id)
+  end
+  
 end
